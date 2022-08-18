4299e7568be727623d083ac0d185bc75094e687c
LudvigThunberg <ludvigthunberg@gmail.com>
2022-08-18 10:02:13 +0200

    commit

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,73 +1,73 @@
-import { useEffect, useState } from "react"
-import { IPost } from "../models/IPost"
-import { Post } from "../models/Post"
-import getPosts from "../services/getAllPostsService"
-import sendPostToDB from "../services/savePostToDB"
-import { CreatePost } from "./CreatePost"
-import { SinglePost } from "./SinglePost"
-import "../scss/Posts.scss"
-import { ErrorMessage } from "./ErrorMessage"
+import { useEffect, useState } from "react";
+import { IPost } from "../models/IPost";
+import { Post } from "../models/Post";
+import getPosts from "../services/getAllPostsService";
+import sendPostToDB from "../services/savePostToDB";
+import { CreatePost } from "./CreatePost";
+import { SinglePost } from "./SinglePost";
+import "../scss/Posts.scss";
+import { ErrorMessage } from "./ErrorMessage";
 
 export const Posts = () => {
-    //COMPONENT STATES
-    const [allPosts, setAllPosts] = useState<IPost[]>([])
-    const [singlePost, setSinglePost] = useState<Post>({post: "", name: ""})
-    const [error, setError] = useState(false);
+  //COMPONENT STATES
+  const [allPosts, setAllPosts] = useState<IPost[]>([]);
+  const [singlePost, setSinglePost] = useState<Post>({ post: "", name: "" });
+  const [error, setError] = useState(false);
 
-    //SEND NEW POST TO DB, RETURN POST AND ADD TO ALLPOSTS
-    useEffect(() => {
-        function sendPost() {
-            if(singlePost.name.length !== 0){
-                sendPostToDB(singlePost)
-                .then((post)=>{
-                    setAllPosts(allPosts => [...allPosts, post])
-                })
-                .catch(error=>{
-                    console.log("Error!: ", error);
-                    setError(true)
-                })
-            }
-        }
-        sendPost();
-    }, [singlePost])
-    
-    //GET ALL POSTS FROM DB
-    useEffect(()=>{
-            getPosts()
-            .then((response)=>{
-                setAllPosts(response);      
-            }).catch(error =>{
-                console.log("Error!: ", error);
-                setError(true)
-            })
-    }, [])
+  //SEND NEW POST TO DB, RETURN POST AND ADD TO ALLPOSTS
+  useEffect(() => {
+    const sendPost = async () => {
+      if (singlePost.name.length !== 0) {
+        const post = await sendPostToDB(singlePost);
+        setAllPosts((allPosts) => [...allPosts, post]);
+      }
+    };
+    sendPost();
+  }, [singlePost]);
 
-    //SET SINGLE POST STATE
-    const createNewPost = (name: string, post: string) => {
-        setSinglePost(new Post(name, post))
-    }
-    
-    const resetErrorMessage = () =>{
-        setError(false)
-    }
-    
-    //LOOP SINGLE POST COMPONENT HTML
-    let reversedAllPost = allPosts.map(post => post).reverse();
-    let singlePostToComponent = 
-        reversedAllPost.map((p) =>{
-            return(
-                <SinglePost key={p._id} post={p} ></SinglePost>
-            )
-        })
-    
-    return(
-        <>
-        <div className="all-container">
+  //GET ALL POSTS FROM DB
+  useEffect(() => {
+    getPosts()
+      .then((response) => {
+        setAllPosts(response);
+      })
+      .catch((error) => {
+        console.log("Error!: ", error);
+        setError(true);
+      });
+  }, []);
+
+  //SET SINGLE POST STATE
+  const createNewPost = (name: string, post: string) => {
+    setSinglePost(new Post(name, post));
+  };
+
+  const resetErrorMessage = () => {
+    setError(false);
+  };
+
+  //LOOP SINGLE POST COMPONENT HTML
+  let reversedAllPost = allPosts.map((post) => post).reverse();
+  // let singlePostToComponent =
+  //     reversedAllPost.map((p) =>{
+  //         return(
+  //             <SinglePost key={p._id} post={p} ></SinglePost>
+  //         )
+  //     })
+
+  return (
+    <>
+      <div className="all-container">
         <h1>Posts and comments</h1>
-            <CreatePost createNewPost={createNewPost} ></CreatePost>
-            {singlePostToComponent}
-        </div>
-        <ErrorMessage resetErrorMessage={resetErrorMessage} error={error} ></ErrorMessage>
-        </>
-    )
-}
+        <CreatePost createNewPost={createNewPost}></CreatePost>
+        {reversedAllPost.map((p) => (
+          <SinglePost key={p._id} post={p} />
+        ))}
+      </div>
+      <ErrorMessage
+        resetErrorMessage={resetErrorMessage}
+        error={error}
+      ></ErrorMessage>
+    </>
+  );
+};
--- src/components/SinglePost.tsx
+++ src/components/SinglePost.tsx
@@ -1,86 +1,88 @@
-import { IPost } from "../models/IPost"
-import { CreateComment } from "./CreateComment"
-import { Comment } from "../models/Comment"
-import sendCommentToDB from "../services/saveCommentToDb"
-import { useEffect, useState } from "react"
-import { IComment } from "../models/IComment"
-import getCommentsByPostId from "../services/getCommentsByPostId"
-import { SingleComment } from "./SingleComment"
-import {ErrorMessage} from "./ErrorMessage"
-import "../scss/SinglePost.scss"
-interface ISinglePostProps{
-    post: IPost,
-
+import { IPost } from "../models/IPost";
+import { CreateComment } from "./CreateComment";
+import { Comment } from "../models/Comment";
+import sendCommentToDB from "../services/saveCommentToDb";
+import { useEffect, useState } from "react";
+import { IComment } from "../models/IComment";
+import getCommentsByPostId from "../services/getCommentsByPostId";
+import { SingleComment } from "./SingleComment";
+import { ErrorMessage } from "./ErrorMessage";
+import "../scss/SinglePost.scss";
+interface ISinglePostProps {
+  post: IPost;
 }
 
-export const SinglePost = (props: ISinglePostProps) => {
-    //ALL COPMONENT STATES
-    const [allComments, setAllComments] = useState<IComment[]>([])
-    const [isActive, setIsActive] = useState(false);
-    const [error, setError] = useState(false);
-    const [comment, setComment] = useState<Comment>({
-        message: "",
-        name: "",
-        postId: "",
-    })
-    
-    //SEND NEW COMMENT TO DB, RETURN COMMENT AND ADD TO ALLCOMMENTS
-    useEffect(() => {
-        function sendComment(){
-            if(comment.message.length !== 0){
-                sendCommentToDB(comment)
-                .then((comment)=>{
-                    setAllComments(allComments => [...allComments, comment])
-                }).catch(error => {
-                    console.log("Error!: ", error);
-                    setError(true)
-                })
-            }
-        }
-        sendComment();
-    },[comment])
-    
-    //GET ALL COMMENTS by postId FROM DB
-    const getComments = () => {
-        getCommentsByPostId(props.post._id)
-        .then((response)=>{
-            setAllComments(response)
-            setIsActive(!isActive)
-        }).catch((error) =>{
+export const SinglePost = ({ post }: ISinglePostProps) => {
+  //ALL COPMONENT STATES
+  const [allComments, setAllComments] = useState<IComment[]>([]);
+  const [isActive, setIsActive] = useState(false);
+  const [error, setError] = useState(false);
+  const [comment, setComment] = useState<Comment>({
+    message: "",
+    name: "",
+    postId: "",
+  });
+
+  //SEND NEW COMMENT TO DB, RETURN COMMENT AND ADD TO ALLCOMMENTS
+  useEffect(() => {
+    function sendComment() {
+      if (comment.message.length !== 0) {
+        sendCommentToDB(comment)
+          .then((comment) => {
+            setAllComments((allComments) => [...allComments, comment]);
+          })
+          .catch((error) => {
             console.log("Error!: ", error);
-            setError(true)
-        })
+            setError(true);
+          });
+      }
     }
+    sendComment();
+  }, [comment]);
 
-    // SET LOCAL STATES
-    const addComment = (message: string, name: string) => {
-        setComment(new Comment(message, name, props.post._id))
-    }
+  //GET ALL COMMENTS by postId FROM DB
+  const getComments = () => {
+    getCommentsByPostId(post._id)
+      .then((response) => {
+        setAllComments(response);
+        setIsActive(!isActive);
+      })
+      .catch((error) => {
+        console.log("Error!: ", error);
+        setError(true);
+      });
+  };
 
-    const resetErrorMessage = () =>{
-        setError(false)
-    }
+  // SET LOCAL STATES
+  const addComment = (message: string, name: string) => {
+    setComment(new Comment(message, name, post._id));
+  };
 
-    // LOOP SINGLE COMMENT COMPONENT
-    let comments = allComments.map((comment)=>{
-        return(
-            <SingleComment key={comment._id} comment={comment}></SingleComment> 
-        )
-    })
-    
-    let commentButton = <button onClick={getComments}>Comments</button>
-    let createComment = <CreateComment addComment={addComment}></CreateComment>
+  const resetErrorMessage = () => {
+    setError(false);
+  };
 
-    return(
-        <div className="container">
-            <div className="comment-container">
-                <p className="name">{props.post.name}</p>
-                <p className="post">{props.post.post}</p>
-                {!isActive && commentButton}
-                {isActive && createComment}
-                {comments}
-                <ErrorMessage resetErrorMessage={resetErrorMessage} error={error} ></ErrorMessage>
-            </div>
-        </div>
-    )
-}
+  // LOOP SINGLE COMMENT COMPONENT
+  let comments = allComments.map((comment) => {
+    return <SingleComment key={comment._id} comment={comment}></SingleComment>;
+  });
+
+  let commentButton = <button onClick={getComments}>Comments</button>;
+  let createComment = <CreateComment addComment={addComment}></CreateComment>;
+
+  return (
+    <div className="container">
+      <div className="comment-container">
+        <p className="name">{post.name}</p>
+        <p className="post">{post.post}</p>
+        {!isActive && commentButton}
+        {isActive && createComment}
+        {comments}
+        <ErrorMessage
+          resetErrorMessage={resetErrorMessage}
+          error={error}
+        ></ErrorMessage>
+      </div>
+    </div>
+  );
+};
